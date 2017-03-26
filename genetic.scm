5d8610f5d46d8c97a4c48ba79451b2b008d69361
nathaniel <nathaniel@marginalia.be>
2017-03-26 23:19:41 +0200

    lol

--- genetic.scm
+++ genetic.scm
@@ -1,24 +1,8 @@
 (use srfi-1)
 
-(define *operators* (make-parameter
-		     (lambda ()
-		       (random-elt '(* + - /)))))
-
-(define math-operators
-  (lambda ()
-    (random-elt '(* + - /))))
-
-(define *variables* (make-parameter '(x)))
-
-(define *leaves* (make-parameter
-		  (lambda ()
-		    (random 10))))
-
 (define (random-elt seq)
   (when (not (null? seq))
 	(list-ref seq (random (length seq)))))
-
-;; TODO: write random-cond
 
 (define-syntax random-if
   (syntax-rules ()
@@ -35,35 +19,45 @@
        (when (< (/ r 10000) index)
 	   conseq)))))
 
-(define (random-form operators variables #!optional (leaves (*leaves*)) (max-depth 10))
+(define (random-form operators leaves #!optional (max-depth 20))
   (let ((operand (lambda ()
 		   (random-if (if (> max-depth 0) 1/2 0)
-			      (random-form operators variables leaves (- max-depth 1))
-			      (random-if (if (> (length variables) 0)
-					     1/2
-					     1)
-					 (leaves)
-					 (random-elt variables))))))
+			      (random-form operators leaves (- max-depth 1))
+			      (leaves)))))
        (list (operators) (operand) (operand))))
 
 
 ;; do this with syntax rules?
 
-(define (run-form form variables input)
+
+(define-syntax build-form
+  (syntax-rules ()
+    ((build-form (var ...) body)
+     (lambda (var ...) body))))
+
+(define-syntax run-form
+  (syntax-rules ()
+    ((run-form form vars input)
+     (handle-exceptions
+      exn (begin (print ((condition-property-accessor 'exn 'message) exn)) #f)
+     ((eval `(build-form ,vars ,form)) input)))))
+
+(define (old-run-form form variables input)
   (handle-exceptions
-   exn #f
+   exn (begin (print ((condition-property-accessor 'exn 'message) exn)) #f)
    (eval `((lambda ,variables ,form) ,@input))))
 
-(define (initial-population operators variables #!optional (size 100))
-  (list-tabulate size (lambda (x) (random-form operators variables))))
+(define (initial-population operators leaves #!optional (size 100))
+  (list-tabulate size (lambda (x) (random-form operators leaves))))
 
-(define (fitness form variables fitness-fn test-inputs)
+(define (fitness form variables fitness-fn test-inputs compare)
   (reduce * 1
 	  (map (lambda (input)
 		 (let ((result (run-form form variables input)))
 		   (if result
-		       (/ 1 (+ 1 (abs (- result (fitness-fn input)))))
+		       (compare result (fitness-fn input))
 		       0)))
+		 
 	       test-inputs)))
 
 (define (random-node-split form #!optional (cont (lambda (node) node)))
@@ -103,22 +97,24 @@
     (cons ((cdr split1) (car split2))
 	  ((cdr split2) (car split1))))))
 
-(define (mutate form operators variables)
+(define (mutate form operators variables leaves)
   (let ((split (random-node-split form))
-	(nform (random-form operators variables)))
+	(nform (random-form operators leaves)))
     ((cdr split) nform)))
 
-(define (evaluate-population population variables fitness-fn test-inputs)
+(define (evaluate-population population variables fitness-fn test-inputs
+			     #!optional (compare (lambda (a b) (/ 1 (+ 1 (abs (- a b)))))))
   (sort
-   (map (lambda (f) (cons (fitness f variables fitness-fn test-inputs)
+   (map (lambda (f) (cons (fitness f variables fitness-fn test-inputs compare)
 			  f))
 	population)
    (lambda (a b) (> (car a) (car b)))))
 
-(define (advance-generation population operators variables fitness-fn test-inputs
-			    #!optional (max-population 100))
-  (let ((epop (evaluate-population population variables fitness-fn test-inputs)))
-    (format #t "BEST: ~A~%" (car (car epop)))
+(define (advance-generation population operators variables leaves fitness-fn test-inputs
+			    #!optional (max-population 100) compare)
+
+  (let ((epop (evaluate-population population variables fitness-fn test-inputs compare)))
+    (format #t "BEST of ~A: ~A~%" (length epop) (car (car epop)))
     (fold (lambda (f accum)
 	    (let ((fitness (car f))
 		  (form (cdr f)))
@@ -131,56 +127,17 @@
 		 (random-when fitness
 			      (random-if 0.9
 					 (cross-over form (cdr (random-elt epop)))
-					 (mutate form operators variables)))
-		 (random-when 0.02 (random-form operators variables)))))))
+					 (mutate form operators variables leaves)))
+		 (random-when 0.02 (random-form operators leaves)))))))
 	  '()
 	      
 	 (take epop max-population))))
 
-
-(define (test)
-  (let ((f1 (random-form (*operators*) (*variables*)))
-	(f2  (random-form (*operators*) (*variables*))))
-    (print (cross-over f1 f2))))
-  ;; (run-form (random-form (*operators*) (*variables*)) (*variables*) '(5)))
-
-;; (fitness (car (initial-population (*operators*) (*variables*) 2)) (*variables*) (lambda (xl) (* (car xl) 10)) '((1) (2) (3) (4)))
-
-(define (run population operators variables fitness-fn test-inputs n #!optional (max-population 10))
+(define (run population operators variables leaves fitness-fn test-inputs n
+	     #!optional (max-population 100) compare)
   (let recur ((n n)
 	      (pop population))
     (if (> n 0)
 	(recur (- n 1)
-	       (advance-generation pop operators variables fitness-fn test-inputs))
-	(evaluate-population pop variables fitness-fn test-inputs))))
-
-(define population (initial-population (*operators*) (*variables*) 100))
-
-(define (test-evaluate pop)
-  (evaluate-population pop (*variables*)
-		       (lambda (xl) (* (car xl) 10))
-		      '((1) (2) (3))))
-
-(define (test-advance pop)
-  (advance-generation pop (*operators*) (*variables*)
-		      (lambda (xl) (* (car xl) 10))
-		      '((1) (2) (3))))
-
-(define (test n)
-  (run  (initial-population (*operators*) (*variables*) 100)
-	(*operators*) (*variables*)
-		      (lambda (xl) (* (+  (* 3 (car xl)) (car xl) (car xl))))
-		      '((1) (2) (3)) n 10))
-
-(define (test-form)
-   (random-form (*operators*) (*variables*)))
-
-(define (test-crossover n)
-  (let recur ((n n)
-	      (t1 (test-form))
-	      (t2 (test-form)))
-    (if (> n 0)
-	(let ((crosses (cross-over t1 t2)))
-	  (map print crosses) (newline)
-	  (recur (- n 1) (car crosses) (cdr crosses)))
-	'done)))
+	       (advance-generation pop operators variables leaves fitness-fn test-inputs max-population compare))
+	(evaluate-population pop variables fitness-fn test-inputs compare))))
--- test.scm
+++ test.scm
@@ -1,42 +1,51 @@
 (use csv csv-string)
 (load "genetic.scm")
 
+(define math-operators
+  (lambda ()
+    (random-elt '(* + - /))))
 
-(define datafile (with-input-from-file "data/TFdata20170315.csv" read-string))
+(define *variables* '(x))
 
-(define parse (csv-parser #\,))
+(define (leaves)
+  (random 10))
 
-(define data
-  (cdr (map csv-record->list (parse datafile))))
+(define (test)
+  (let ((f1 (random-form math-operators *variables*))
+	(f2  (random-form math-operators *variables*)))
+    (print (cross-over f1 f2))))
+  ;; (run-form (random-form (*operators*) (*variables*)) (*variables*) '(5)))
 
-(define ECOICOP6s (map second data))
+;; (fitness (car (initial-population (*operators*) (*variables*) 2)) (*variables*) (lambda (xl) (* (car xl) 10)) '((1) (2) (3) (4)))
 
-(define retailer-descriptions (map seventh data))
 
-(define retailer-descriptions-words
-  (map string-split retailer-descriptions))
+(define (test-evaluate pop)
+  (evaluate-population pop *variables*
+		       (lambda (xl) (* (car xl) 10))
+		      '((1) (2) (3))))
 
-(define word-bank
-  (delete-duplicates
-   (join retailer-descriptions-words)))
+(define (test-advance pop)
+  (advance-generation pop math-operators *variables*
+		      (lambda (xl) (* (car xl) 10))
+		      '((1) (2) (3))))
 
-(define feature-lists
-  (map (lambda (desc)
-	 (map (lambda (word)
-		(and (memq word desc) #t))
-	      word-bank))
-       retailer-descriptions-words))
+(define (test n)
+  (run  (initial-population math-operators *variables* 100)
+	math-operators *variables*
+		      (lambda (xl) (* (+  (* 3 (car xl)) (car xl) (car xl))))
+		      '((1) (2) (3)) n 10))
 
-(define (if-test test)
-  (if test
-      (lambda (x y) x)
-      (lambda (x y) y)))
+(define (test-form)
+   (random-form math-operators *variables*))
 
-(define (equal-test var x)
-  `(if-test (equal? ,var ,x)))
+(define (test-crossover n)
+  (let recur ((n n)
+	      (t1 (test-form))
+	      (t2 (test-form)))
+    (if (> n 0)
+	(let ((crosses (cross-over t1 t2)))
+	  (map print crosses) (newline)
+	  (recur (- n 1) (car crosses) (cdr crosses)))
+	'done)))
 
-(define (random-equal-test elts var)
-  (equal-test var (random-elt elts)))
-
-(*operators* (lambda ()
-	       (equal-test 'x "abc")))
+(define population (initial-population (*operators*) (*variables*) 100))
